0d6df86e73713a052be6ea96a6675591d2380c5e
DiscordRPC <lukashammerprivate@gmail.com>
2022-11-27 01:59:58 +0000

    ref: publish.bat bash dependency removed

--- publish.bat
+++ publish.bat
@@ -1,17 +1,4 @@
-#!/bin/bash
-
-cd ./build
+@echo off
 git add .
-
-# Get commit message from user
-read MSG
-
-# Use generic commit message if none is given
-if [ -z "$MSG" ]
-then
-    MSG="Published"
-fi
-
-# Commit and push
-git commit -m "$MSG"
+git commit -m "Published"
 git push -u origin master
